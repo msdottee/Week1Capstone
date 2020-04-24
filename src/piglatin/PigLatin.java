b981f262cb8a150992c45c750572f2e22c83db88
ashley <ashleykubera@gmail.com>
2020-04-24 17:00:17 -0400

    mostly finished lab

--- src/piglatin/PigLatin.java
+++ src/piglatin/PigLatin.java
@@ -1,10 +1,67 @@
 package piglatin;
 
+import java.util.Scanner;
+
 public class PigLatin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean translate = true;
+		
+		System.out.println("Welcome to the Pig Latin Translator!");
+		
+		System.out.println();
+		
+		whileLoop(translate);
 
+		}
+		
+		private static void whileLoop(boolean translate) {
+			try (Scanner scnr = new Scanner(System.in)) {
+				while (translate) {
+
+					System.out.println("Enter a line to be translated: ");
+					
+					String userString = scnr.nextLine();
+					
+					System.out.println(stringBuilderMethod(userString));
+					
+					System.out.println();
+					
+					System.out.println("Translate another line? (y/n):");
+
+					String answer = scnr.nextLine().toLowerCase();
+
+					if (answer.equals("n")) {
+						translate = false;
+						System.out.println();
+ 					} else if (answer.isEmpty()) {
+ 						System.out.println("Please enter a sentence or n/N to exit.");
+ 					}
+				}
+			}
+		}
+		
+		private static String stringBuilderMethod(String userString) {
+			StringBuilder pigLatin = new StringBuilder();
+			
+			String[] stringSplit = userString.split(" ");
+			
+			for (String seperateWords : stringSplit) {
+				willItPigLatin(pigLatin, seperateWords);
+			}
+			return pigLatin.toString();
+		}
+
+		private static void willItPigLatin(StringBuilder pigLatin, String seperateWords) {
+			if (startsWithVowel(seperateWords)) {
+				pigLatin.append(seperateWords).append("way ");
+			} else {
+				pigLatin.append(seperateWords.substring(1)).append(seperateWords.charAt(0)).append("ay ");
+			}
+		}
+		
+		private static boolean startsWithVowel(String word) {
+			return "AEIOUaeiou".indexOf(word.charAt(0)) != -1;
+		}
 	}
 
-}
